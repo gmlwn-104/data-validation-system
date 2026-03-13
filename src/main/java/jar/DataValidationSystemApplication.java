fa84a34be8239ceb19230bd01e7e1287dcca0838
gmlwn104 <10heeju04@gmail.com>
2026-03-13 20:15:10 +0900

    Initial Spring Boot project setup

---
+++ src/main/java/jar/DataValidationSystemApplication.java
@@ -0,0 +1,13 @@
+package jar;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class DataValidationSystemApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(DataValidationSystemApplication.class, args);
+	}
+
+}
